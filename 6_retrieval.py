402779f7a5ed9a0e636434ed862f3d0bfec5728d
Shweta-tech712 <itzzshwetu@gmail.com>
2026-03-08 20:56:06 +0530

    Delete 6_retrieval.py

--- 6_retrieval.py
+++
@@ -1,6 +0,0 @@
-# RAG-based Policy Retrieval
-
-def retrieve_policy(query, chunks, model, index, k=3):
-    query_vector = model.encode([query])
-    distances, indices = index.search(query_vector, k)
-    return [chunks[i].page_content for i in indices[0]]
