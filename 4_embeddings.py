7fd65afae682870d0e88c698e9bd03f97e932f09
Shweta-tech712 <itzzshwetu@gmail.com>
2026-03-08 20:55:35 +0530

    Delete 4_embeddings.py

--- 4_embeddings.py
+++
@@ -1,9 +0,0 @@
-# Embedding Creation
-
-from sentence_transformers import SentenceTransformer
-
-def create_embeddings(chunks):
-    model = SentenceTransformer("all-MiniLM-L6-v2")
-    texts = [chunk.page_content for chunk in chunks]
-    embeddings = model.encode(texts, show_progress_bar=True)
-    return embeddings, model    
