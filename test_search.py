e8cb13ef6c7e18e4215c77f25ae251d91b7780ab
Adnan <mdadnan3710@gmail.com>
2026-03-31 21:31:47 +0530

    Implement visa-related search functionality

--- test_search.py
+++ test_search.py
@@ -1,1 +1,13 @@
+from faiss_search import VisaSemanticSearch
 
+engine = VisaSemanticSearch()
+
+query = input("Please enter your visa-related question: ")
+
+results = engine.search(query)
+
+print("\nTOP RELEVANT POLICY FOUND:\n")
+
+for r in results:
+    print(r)
+    print("-" * 60)
